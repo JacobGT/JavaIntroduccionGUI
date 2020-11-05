ced2b1d1165bf939c1ffa887342da56d77a843db
JacobGT <42787753+JacobGT@users.noreply.github.com>
2020-11-05 13:10:54 -0600

    Update GUI.java
    se comentan las lineas donde instanciamos la segunda ventana

--- GUI.java
+++ GUI.java
@@ -8,15 +8,8 @@ public class GUI {
         Ventana v1 = new Ventana(); //Instanciamos un objeto ventana
         v1.setVisible(false);// Lo hacemos visible
         
-        Welcome bienvenido = new Welcome();
-        //bienvenido.show();
-        
-        FrmCalculadora calc = new FrmCalculadora();
-        calc.setLocation(1300, 350);
-        //calc.show();
-        
-        ActionListeners aL1 = new ActionListeners();
-        aL1.setVisible(true);
+        ActionListeners aL1 = new ActionListeners(); //Instanciamos el objeto ActionListeners
+        aL1.setVisible(true);//lo hacemos visible
     }
     
 }
